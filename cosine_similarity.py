764a8a24f6b6fac1c6c95fdcce36876d4318e66c
Faseeu <faseuu92@gmail.com>
2026-06-27 11:06:23 +0500

    added cosine similarity function

---
+++ cosine_similarity.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+
+def cosine_similarity(query_embed, chunk_embed):
+
+    A = query_embed
+    B = chunk_embed
+
+    dot_product = sum(a * b for a, b in zip(query_embed, chunk_embed))
+    magOfA = magnitude(A)
+    magOfB = magnitude(B)
+
+    return dot_product / magOfA * magOfB
+
+
+def magnitude(embed):
+
+    return sqrt(sum(e**2 for e in embed))
--- embedding.py
+++ embedding.py
@@ -1,2 +1,18 @@
-def embed(text):
-    
+import requests
+import numpy as np
+
+
+def embed(texts: list[str], task: str) -> list[list[float]]:
+
+    result = requests.post(
+        url="https://api.jina.ai/v1/embeddings",
+        headers={"Authorization": "Bearer YOUR_KEY"},
+        json={
+            "model": "jina-embeddings-v4",
+            "task": task,
+            "input": texts,
+            "dimensions": 512,
+        },
+    ).json()
+
+    return 
