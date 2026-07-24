0961b077033e85305ff5e21385f09b0446b5a425
Faseeu <faseuu92@gmail.com>
2026-07-24 10:03:55 +0500

    Added a few new instructions for the new answer structure

--- prompt_builder.py
+++ prompt_builder.py
@@ -3,10 +3,14 @@ def prompt_builder(query, chunks, memory):
     Use ONLY the context provided
     Don't make things up
     If the answer isn't there, say so"""
-    rag = "\n\n".join(chunks)
+    rag = "\n\n".join(f"[chunk {i}]: {chunk}" for i, chunk in enumerate(chunks))
+    # rag = "\n\n".join(chunks)
     # rag = "\n\n".join(chunk for chunk in chunks)
     prompt = f"""
     You are a STRICT RAG assistant. You have NO external knowledge.
+    But from the data provided in the context you should
+    try your best to answer the user properly instead of just
+    summarizing from the context 
 
     
     
@@ -14,11 +18,23 @@ def prompt_builder(query, chunks, memory):
     ONLY the context provided below.
     If the answer is not in the context, 
     say "I don't know."
+    Make sure you also try to tell the user what type of question 
+    he should try to ask to get the proper answer,
+    from the context
+    instead of just answering in a blunt i dont know that
+    might leave the user confused.
     If the information is not explicitly in the context,
     you MUST NOT mention it, even if you know it's true.
     Every sentence you write must pass this test:
     "Which exact part of the CONTEXT supports this?"
     If no part supports it, delete the sentence.
+
+    Always cite resources
+    Make sure to give every source
+    From where you took anything
+    ANd make sure to give the sources inside a list of dicts
+    Where the dict uses the paragrapgh id for the key and
+    the source line as the value
 
     Being helpful by adding outside information is a failure.
 
@@ -31,7 +47,7 @@ def prompt_builder(query, chunks, memory):
     - Do not be helpful beyond what is written in the context.
     - Do not expand, explain, or elaborate using outside knowledge.
     - If you violate these rules, you are hallucinating and the answer is wrong.
-
+    - Always cite the sources from where the data in your answers was taken
     {strict}
     --- CONVERSATION MEMORY ---
     {memory}
@@ -55,4 +71,3 @@ def prompt_builder(query, chunks, memory):
 
 # if __name__ == "__main__":
 #     prompt = prompt_builder("Suggest me a new method to build a strong identity daily.",)
-
